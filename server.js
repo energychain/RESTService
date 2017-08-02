b2555d8daf97f178dc01ce2d6aaa35f43237a6c9
Thorsten Zoerner <me@thorsten-zoerner.com>
2017-08-02 14:24:48 +0200

    Updated BO Loader

--- server.js
+++ server.js
@@ -261,7 +261,7 @@ startStopDaemon(options, function() {
 				  var res={};
 				  if(charge.paid) {
 						console.log("Payment Account",node.wallet.address);
-						node.stromkonto("0xf2E3FAB8c3A82388EFd9B5fd9F4610509c4855F4").then(function(sko) {
+						node.stromkonto("0x19BF166624F485f191d82900a5B7bc22Be569895").then(function(sko) {
 							sko.addTx("0x0013ab4e15A14B97D517e75fb7F6f9fF13514e30",request.payload.account,request.payload.amount,0).then(function(tx) {
 								res.tx=tx;							 
 							 reply(JSON.stringify(res));
