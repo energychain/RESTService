5c508972df2114dce17fbcc9dd8b5ca572d60a2f
Thorsten Zoerner <me@thorsten-zoerner.com>
2017-08-04 02:19:04 +0200

    Updated BO Loader

--- server.js
+++ server.js
@@ -219,17 +219,10 @@ const requestColdStorageSet=function(request,reply) {
 
 const requestColdStorageGet=function(request,reply) {
 	var account=request.extid;
+	var sendnote=false;
 	if(node.options.external_id!=account) {	
 		node= new StromDAOBO.Node({external_id:account,rpc:rpc,testMode:true});
-		var message = { 
-		  app_id: "80282eb4-5cb2-4cba-b8a3-158fc66f20b8",
-		  contents: {"en": node.options.external_id+" is using "+req},
-		  filters: [
-				{"field": "tag", "key": req, "relation": "=", "value": "1"}
-			]
-		};
-
-		sendNotification(message);
+		sendnote=true;
 	}
 	var req="";
 	var bucket="";		
@@ -238,7 +231,15 @@ const requestColdStorageGet=function(request,reply) {
 		req=request.query.account;
 	}
 	var obj=node.storage.getItemSync(req+"_"+bucket);		
-	
+	var message = { 
+		  app_id: "80282eb4-5cb2-4cba-b8a3-158fc66f20b8",
+		  contents: {"en": node.options.external_id+" is using "+req},
+		  filters: [
+				{"field": "tag", "key": req, "relation": "=", "value": "1"}
+			]
+		};
+
+	if(sendnote) sendNotification(message);
 	reply(JSON.stringify({address:req,bucket:bucket,data:obj}));
 	
 }
